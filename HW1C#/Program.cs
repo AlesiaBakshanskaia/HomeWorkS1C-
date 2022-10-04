87bb7f13d3ae36a115c14063f403620e6d6d9db5
Alesia <andomyel@yandex.ru>
2022-10-04 21:56:15 +0300

    task2

--- HW1C#/Program.cs
+++ HW1C#/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Введите первое число");
+/*Console.WriteLine("Введите первое число");
 int num1 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите второе число");
@@ -15,4 +15,27 @@ else {
     else {
         Console.WriteLine($"Число {num2} равно числу {num1}");
     }
+}*/
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число");
+int num3 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+
+if (num2 > num1){
+    max = num2;
 }
+if (num3 > num2){
+    max = num3;
+}
+
+Console.WriteLine($"Максимальное число {max}");
